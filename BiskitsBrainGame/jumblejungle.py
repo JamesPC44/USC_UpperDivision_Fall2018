34e959228ba9d6e5fa9154623058e93e5fa66b77
Tori McQuinn <tmcquinn@swampfox.com>
2018-10-23 09:26:57 -0400

    Changing to ordered dict

--- BiskitsBrainGame/jumblejungle.py
+++ BiskitsBrainGame/jumblejungle.py
@@ -1,18 +1,64 @@
 import time
-
-start = time.time()
+import collections
 
 str1 = input()
 str2 = input()
+dic1 = collections.Ordereddic()
+dic2 = collections.Ordereddic()
+ 
+dic1['a'] = 0
+dic1['b'] = 0
+dic1['c'] = 0
+dic1['d'] = 0
+dic1['e'] = 0
+dic1['f'] = 0
+dic1['g'] = 0
+dic1['h'] = 0
+dic1['i'] = 0
+dic1['j'] = 0
+dic1['k'] = 0
+dic1['l'] = 0
+dic1['m'] = 0
+dic1['n'] = 0
+dic1['o'] = 0
+dic1['p'] = 0
+dic1['q'] = 0
+dic1['r'] = 0
+dic1['s'] = 0
+dic1['t'] = 0
+dic1['u'] = 0
+dic1['v'] = 0
+dic1['w'] = 0
+dic1['x'] = 0
+dic1['y'] = 0
+dic1['z'] = 0
 
-# Map for each letter of the alphabet for each string
-dic1 = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0,
-        'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 
-        't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0}
-
-dic2 = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0,
-        'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 
-        't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0}
+dic2['a'] = 0
+dic2['b'] = 0
+dic2['c'] = 0
+dic2['d'] = 0
+dic2['e'] = 0
+dic2['f'] = 0
+dic2['g'] = 0
+dic2['h'] = 0
+dic2['i'] = 0
+dic2['j'] = 0
+dic2['k'] = 0
+dic2['l'] = 0
+dic2['m'] = 0
+dic2['n'] = 0
+dic2['o'] = 0
+dic2['p'] = 0
+dic2['q'] = 0
+dic2['r'] = 0
+dic2['s'] = 0
+dic2['t'] = 0
+dic2['u'] = 0
+dic2['v'] = 0
+dic2['w'] = 0
+dic2['x'] = 0
+dic2['y'] = 0
+dic2['z'] = 0
 
 stack1 = []
 stack2 = []
@@ -23,11 +69,7 @@ for x in str1:
 for x in str2:
     dic2[x] = dic2[x] + 1
 
-print ("dic 1")
-print (dic2)
-print ("dic 2")
-print (dic1)
-# At this point each string is in its own dictionary
+# At this point each string is in its own dicionary
 for key, value in dic1.items():
     if dic1[key] >= dic2[key]:
         dic1[key] = dic1[key] - dic2[key]
@@ -39,11 +81,6 @@ for key, value in dic1.items():
     # In java this is equivalent to Collections.nCopies(number, element)
     stack1 = stack1 + ([key] * dic1[key])
     stack2 = stack2 + ([key] * dic2[key])
-
-print ("stack1")
-print (stack1)
-print ("stack2")
-print (stack2)
 
 if len(stack1) == 0 and len(stack2) == 0:
     print('AC JUMBLE')
@@ -65,6 +102,3 @@ else:
         print(retval)
     else:
         print(retval)
-
-
-print (time.time() - start)
